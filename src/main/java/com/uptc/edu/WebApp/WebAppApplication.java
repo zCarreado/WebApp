0e4d7cc106f0f78148d5c707e1c104fbae146ba6
zCarreado <sergio.perez06@uptc.edu.co>
2026-03-07 18:36:30 -0500

    first commit

---
+++ src/main/java/com/uptc/edu/WebApp/WebAppApplication.java
@@ -0,0 +1,13 @@
+package com.uptc.edu.WebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppApplication.class, args);
+	}
+
+}
